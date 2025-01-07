035136b67c6e73d1abd17ad490a57ef05f1072d5
ananya-gupta28 <lci2021029@iiitl.ac.in>
2025-01-07 18:48:48 +0530

    blogs trial

---
+++ src/app/blogs/page.js
@@ -0,0 +1,10 @@
+
+import { BackgroundGradientAnimationDemo } from "@/components/GradientAnimation";
+import React from "react";
+const Page = () => {
+    return(
+            <BackgroundGradientAnimationDemo/>
+    );
+};
+
+export default Page;
